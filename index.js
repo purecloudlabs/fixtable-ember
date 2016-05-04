55bb87c771917a48159dddfb0d7c414773b1113c
Michael Newman <michael.newman@inin.com>
2016-05-04 13:43:33 -0500

    Add FontAwesome to the add-on. Implement support for a loading indicator.

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
 /* jshint node: true */
 'use strict';
 
+var fs = require('fs');
+var path = require('path');
+
+function importFontAwesome(app) {
+  var fontAwesomePath = path.join(app.bowerDirectory, 'font-awesome');
+
+  var fontAwesomeCssPath = path.join(fontAwesomePath, 'css');
+  app.import({
+    development: path.join(fontAwesomeCssPath, 'font-awesome.css'),
+    production: path.join(fontAwesomeCssPath, 'font-awesome.min.css')
+  });
+
+  var fontAwesomeFontPath = path.join(fontAwesomePath, 'fonts');
+  fs.readdirSync(fontAwesomeFontPath)
+    .forEach(fileName =>
+      app.import(path.join(fontAwesomeFontPath, fileName), { destDir: '/fonts' }));
+}
+
+function importFixtable(app) {
+  var fixtablePath = path.join(app.bowerDirectory, 'fixtable/dist/');
+  app.import({
+    development: path.join(fixtablePath, 'fixtable.css'),
+    production: path.join(fixtablePath, 'fixtable.min.css')
+  });
+  app.import({
+    development: path.join(fixtablePath, 'fixtable.js'),
+    production: path.join(fixtablePath, 'fixtable.min.js')
+  });
+}
+
 module.exports = {
   name: 'fixtable-ember',
 
   included(app) {
     this._super.included(app);
-
-    var fixtablePath = app.bowerDirectory + '/fixtable/dist/';
-    ['fixtable.min.css', 'fixtable.min.js']
-      .forEach(fileName => this.app.import(fixtablePath + fileName));
+    importFontAwesome(app);
+    importFixtable(app);
   }
 };
