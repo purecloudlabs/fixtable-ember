5f92222c5df6b67db16f1c75ba183f2502481e85
Michael Newman <michael.newman@inin.com>
2016-11-07 17:10:00 -0600

    Create ‘editable’ route in dummy app that tests adding rows, deleting rows, editing rows, and swapping out the content array using an ArrayProxy.

---
+++ tests/dummy/app/controllers/editable.js
@@ -0,0 +1,128 @@
+/* global alert */
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  data: Ember.inject.service(),
+
+  // properties for adding a new row
+  newRowName: null,
+  newRowAddress: null,
+  newRowAlignment: null,
+
+  // properties for editing an existing row
+  idToEdit: null,
+  editing: false,
+  editRowName: null,
+  editRowAddress: null,
+  editRowAlignment: null,
+
+  selected: null, // object where keys are IDs of selected rows
+
+  columnDefs: [
+    {
+      key: 'id',
+      header: 'ID',
+      width: 50
+    },
+    {
+      key: 'name',
+      header: 'Name',
+      cellClass: 'name'
+    },
+    {
+      key: 'address',
+      header: 'Address'
+    },
+    {
+      key: 'alignment',
+      header: 'Alignment'
+    }
+  ],
+
+  getEditRow() {
+    let id = this.get('idToEdit');
+    if (!id) {
+      return null;
+    }
+
+    return this.get('model.dataRows').findBy('id', parseInt(id, 10));
+  },
+
+  actions: {
+    addRow() {
+      let dataRows = this.get('model.dataRows');
+      let id = dataRows.get('lastObject.id');
+      id = id ? id + 1 : 1; // increment ID, or start at 1
+
+      let row = Ember.Object.create();
+      row.set('id', id);
+      row.set('name', this.get('newRowName'));
+      row.set('address', this.get('newRowAddress'));
+      row.set('alignment', this.get('newRowAlignment'));
+
+      dataRows.pushObject(row);
+    },
+
+    deleteSelectedRows() {
+      if (this.get('editing')) {
+        alert('You should not delete rows while editing an existing row');
+        return;
+      }
+
+      let dataRows = this.get('model.dataRows');
+      let selected = this.get('selected') || {};
+      for (var i = dataRows.get('length') - 1; i >= 0; i--) {
+        let id = dataRows.objectAt(i).get('id');
+        if (selected[id]) {
+          dataRows.removeAt(i);
+        }
+      }
+
+      this.set('selected', {});
+    },
+
+    editRow() {
+      let row = this.getEditRow();
+      if (!row) {
+        return;
+      }
+
+      this.set('editRowName', row.get('name'));
+      this.set('editRowAddress', row.get('address'));
+      this.set('editRowAlignment', row.get('alignment'));
+      this.set('editing', true);
+    },
+
+    saveEditedRow() {
+      let row = this.getEditRow();
+      if (!row) {
+        return;
+      }
+
+      row.set('name', this.get('editRowName'));
+      row.set('address', this.get('editRowAddress'));
+      row.set('alignment', this.get('editRowAlignment'));
+      this.set('editing', false);
+    },
+
+    resetData() {
+      this.set('model.dataRows.content', this.get('data').getObservableData());
+    },
+
+    onReloadContent() {
+      this.set('selected', {});
+    },
+
+    updateSelection(selectedRows/*, rowIndex*/) {
+      let dataRows = this.get('model.dataRows');
+      let selected = {};
+      Object.keys(selectedRows)
+        .filter(key => selectedRows[key])
+        .forEach(key => {
+          let id = dataRows.objectAt(parseInt(key, 10)).get('id');
+          selected[id] = true;
+        });
+      this.set('selected', selected);
+    }
+  }
+});
