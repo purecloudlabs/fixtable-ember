fba03b923a9f64b84f068909dd52e4be0db64004
Hirschy, Kreigh <Kreigh.Hirschy@inin.com>
2016-11-11 10:26:40 -0500

    Removing Developing add-on

--- index.js
+++ index.js
@@ -21,10 +21,6 @@ function importFixtable(app) {
 module.exports = {
   name: 'fixtable-ember',
 
-  isDevelopingAddon() {
-      return true;
-  },
-
   included(parent) {
     this._super.included.apply(this, arguments);
 
