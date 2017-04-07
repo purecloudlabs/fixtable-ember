003b40d6a9250dc27b00cfdccec5ee1de0c0d7a5
sfanetti <scott.fanetti@inin.com>
2017-04-07 01:11:40 -0400

    fix for test failure

--- tests/integration/components/fixtable-grid-test.js
+++ tests/integration/components/fixtable-grid-test.js
@@ -25,3 +25,32 @@ test('it renders', function(assert) {
   assert.ok(this.$('table').length); // there is a table element
   assert.ok(this.$('.fixtable').length); // there is an element with 'fixtable' class
 });
+
+test('it has clickable rows', function (assert) {
+  const done = assert.async();
+  assert.expect(1);
+
+  this.set('columnDefs', [
+    { key: 'name', header: 'Name', width: 200 },
+    { key: 'address', header: 'Address' }
+  ]);
+  this.set('dataRows', [
+    { name: 'Sherlock Holmes ', address: '221B Baker Street' },
+    { name: 'Dudley Dursley', address: '4 Privet Drive' },
+    { name: 'Hank Hill', address: '84 Rainey Street' },
+    { name: 'Gandalf Stormcrow' },
+  ]);
+
+  let rowClicked = null;
+
+  this.rowClick = function (row) {
+    rowClicked = row;
+    done();
+  };
+
+  this.render(hbs`{{fixtable-grid columns=columnDefs content=dataRows onRowClick=rowClick}}`);
+
+  const $row = this.$('tbody tr')[1];
+  $row.click();
+
+});
