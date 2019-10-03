71ddfa3ffa8414865e7a83e75ebafb867574a2a4
Serge Margovsky <Serge.Margovsky@inin.com>
2019-10-03 15:22:06 -0500

    do not use yarn for ember tre

--- config/ember-try.js
+++ config/ember-try.js
@@ -9,23 +9,8 @@ module.exports = function() {
     getChannelURL('canary'),
   ]).then((urls) => {
     return {
+      useYarn: false,
       scenarios: [
-        {
-          name: 'ember-lts-2.12',
-          npm: {
-            devDependencies: {
-              'ember-source': '~2.12.0'
-            }
-          }
-        },
-        {
-          name: 'ember-lts-2.16',
-          npm: {
-            devDependencies: {
-              'ember-source': '~2.16.0'
-            }
-          }
-        },
         {
           name: 'ember-lts-2.18',
           npm: {
